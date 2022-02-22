8e4473d190a918224983bfa1f430a9db9020dcc3
unknown <muhamadfahmi.f@gmail.com>
2022-02-22 11:54:31 +0700

    initial airbnb app

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#409B94',
+        'secondary': '#2A4441',
+        'elemen1': '#EB9E69',
+        'elemen2': '#F7FFFA',
+      }
+    },
+  },
+  plugins: [],
+}
