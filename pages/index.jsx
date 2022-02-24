176fdc6f115ef2874c2f7dc45dc54f1276770e07
unknown <muhamadfahmi.f@gmail.com>
2022-02-24 23:16:21 +0700

    bind get all rooms

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import { useRouter } from 'next/router'
 import Navbar from '../components/navbar'
 import Sliders from '../components/slidersImage'
+import CurrencyFormat from 'react-currency-format';
+import axios from 'axios'
 
+// local category
 const callouts = [
   {
     name: 'Standart',
@@ -33,47 +36,14 @@ const callouts = [
   },
 ]
 
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://test-upload-s3-rogerdev.s3.ap-southeast-1.amazonaws.com/6216503718eb9324b8213a1f.png',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1580156761943-5bd3b300114e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dmlsbGFnZSUyMGhvdXNlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=600&q=60',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 3,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1633602108759-40f528b59111?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dmlsbGFnZSUyMGhvdXNlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=600&q=60',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 4,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://images.unsplash.com/photo-1556837675-f48f2f7d3882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHZpbGxhZ2UlMjBob3VzZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=600&q=60',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  // More products...
-]
 
-export default function Home() {
+export default function Home(props) {
+
+  const router = useRouter()
+  const rooms = props.rooms.data
+
+  console.log(rooms);
+
   return (
     <>
       <Navbar />
@@ -109,29 +79,29 @@ export default function Home() {
         </div>
       </div>
       <div className='max-w-screen-xl m-auto'>
-        <div className='py-5 px-20 mt-10'>
+        <div className='py-5 px-20 my-10'>
           <h2 className='text-2xl font-semibold select-none'>Yang Paling Menarik</h2>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id} className="group relative">
+            {rooms.map((room) => (
+              <div key={room.room_uid} className="group relative cursor-pointer" onClick={() => { console.log("Click me") }}>
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                   <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
+                    src={room.image}
+                    alt={room.name}
                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
+                      <a>
                         <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
+                        {room.name}
                       </a>
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                    <p className="mt-1 text-sm text-gray-500"></p>
                   </div>
-                  <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                  <p className="text-sm font-medium text-gray-900"><CurrencyFormat value={room.price} displayType={'text'} thousandSeparator={true} prefix={'Rp'} /></p>
                 </div>
               </div>
             ))}
@@ -140,4 +110,14 @@ export default function Home() {
       </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const res = await axios.get("http://18.140.1.124:8081/room");
+  const rooms = await res.data;
+  return {
+    props: {
+      rooms,
+    }, // will be passed to the page component as props
+  };
 }
